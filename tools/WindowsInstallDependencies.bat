fe6ffd31293c8eb862f78f884cbd361bd9eda3a7
Leroy.H.Y <lhy20010403@hotmail.com>
2019-06-21 18:59:02 +0800

    [CI] Added Appveyor CI Config
    Former-commit-id: f7855b6c65f5727fbfd45d2b912047a5cfaa27e7

--- tools/WindowsInstallDependencies.bat
+++ tools/WindowsInstallDependencies.bat
@@ -2,26 +2,14 @@
 REM Install Python
 
 ECHO Downloading Python using wget...
-wget.exe https://www.python.org/ftp/python/3.7.3/python-3.7.3.exe -O %~dp0\python-3.7.3.exe
+%~dp0\wget.exe https://www.python.org/ftp/python/3.7.3/python-3.7.3.exe -O %~dp0\python-3.7.3.exe
 
 REM Get install path 
-CALL :NORMALIZEPATH "..\python37\"
+CALL :NORMALIZEPATH "\python37\"
 ECHO INSTALL_PATH=%RETVAL%
 
 REM Installing Python...
-python-3.7.3.exe /quiet TargetDir=%RETVAL%
-
-REM From: https://stackoverflow.com/a/8185270/8364323
-ECHO Wait for it installing....
-:LOOP
-PSLIST python-3.7.3 >nul 2>&1
-IF ERRORLEVEL 1 (
-  GOTO CONTINUE
-) ELSE (
-  ECHO Waiting for python finish install.
-  TIMEOUT /T 5
-  GOTO LOOP
-)
+start /w "" "%~dp0\python-3.7.3.exe" /quiet TargetDir=%RETVAL%
 
 :CONTINUE
 ECHO Python headers and libs are installed!
