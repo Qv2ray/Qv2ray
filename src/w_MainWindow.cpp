4be965b6b2b3f9ec8a9cbbf980c4ac25615a71d8
Leroy.H.Y <lhy20010403@hotmail.com>
2019-09-04 20:54:03 +0800

    [bug] Temporary fix of Issue: #34
    Former-commit-id: af20994365774ddd2ff4bb32a679fb7a169a5908

--- src/w_MainWindow.cpp
+++ src/w_MainWindow.cpp
@@ -152,7 +152,11 @@ void MainWindow::LoadConnections()
 void MainWindow::save_reload_globalconfig(bool need_restart)
 {
     auto statusText = ui->statusLabel->text();
-    ui->retranslateUi(this);
+    //
+    // A strange bug prevents us to change the UI language `live`ly
+    //    https://github.com/lhy0403/Qv2ray/issues/34
+    //
+    //ui->retranslateUi(this);
     ui->statusLabel->setText(statusText);
     bool isRunning = vinstance->Status == STARTED;
     SaveGlobalConfig();
