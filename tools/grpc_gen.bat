b47197ed9e8606f4c0de172610875711b34724e6
Leroy.H.Y <lhy20010403@hotmail.com>
2019-09-29 23:02:56 +0800

    [Fixed] Fixed Windows CI
    Signed-off-by: Leroy.H.Y <lhy20010403@hotmail.com>

--- tools/grpc_gen.bat
+++ tools/grpc_gen.bat
@@ -1,4 +1,3 @@
-pushd
 cd tools
 echo Extracting files.
 %~dp0\7z.exe -y e %~dp0gRPC-win32.tar.gz -o%~dp0 && %~dp0\7z.exe -y x %~dp0gRPC-win32.tar -o%~dp0\..\libs\gRPC-win32
@@ -11,4 +10,4 @@ echo Generating gRPC file.
 %~dp0\..\libs\gRPC-win32\bin\protoc.exe v2ray_api_commands.proto --grpc_out=%~dp0..\libs\gen --plugin=protoc-gen-grpc="%~dp0..\libs\gRPC-win32\bin\grpc_cpp_plugin.exe" 
 echo Generating proto file.
 %~dp0\..\libs\gRPC-win32\bin\protoc.exe v2ray_api_commands.proto --cpp_out=%~dp0..\libs\gen
-popd
+cd ..
