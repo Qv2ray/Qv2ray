9af3442f8dd62efc19abcaa7a723789a2fedd7ad
Leroy.H.Y <lhy20010403@hotmail.com>
2019-09-30 08:48:32 +0800

    Update grpc_gen.sh

--- tools/grpc_gen.sh
+++ tools/grpc_gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd tools
 mkdir -p ../libs/gen
-protoc --grpc_out=../libs --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ./v2ray_api_commands.proto
-protoc --cpp_out=../libs ./v2ray_api_commands.proto
+protoc --grpc_out=../libs/gen --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ./v2ray_api_commands.proto
+protoc --cpp_out=../libs/gen ./v2ray_api_commands.proto
 
