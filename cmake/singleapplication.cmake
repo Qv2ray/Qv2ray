7b79dc59d1bb86054cc91853d30510056a42148e
Guobang Bi <373318510@qq.com>
2020-03-06 23:31:06 +0800

    add singleapplication.cmake

---
+++ cmake/singleapplication.cmake
@@ -0,0 +1,5 @@
+set(SINGLEAPPLICATION_DIR ${CMAKE_SOURCE_DIR}/3rdparty/SingleApplication)
+set(SINGLEAPPLICATION_SOURCES
+    ${SINGLEAPPLICATION_DIR}/singleapplication.cpp
+    ${SINGLEAPPLICATION_DIR}/singleapplication_p.cpp
+)
