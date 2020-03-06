30179110fe022e616c492d874173baaf4a0d5dca
Guobang Bi <373318510@qq.com>
2020-03-06 22:00:02 +0800

    add qnodeeditor.cmake

---
+++ cmake/qnodeeditor.cmake
@@ -0,0 +1,31 @@
+set(QNODEEDITOR_DIR ${CMAKE_SOURCE_DIR}3rdparty/QNodeEditor)
+add_definitions(-DNODE_EDITOR_SHARED -DNODE_EDITOR_EXPORTS)
+set(QNODEEDITOR_SOURCES
+    ${QNODEEDITOR_DIR}/src/Connection.cpp
+    ${QNODEEDITOR_DIR}/src/ConnectionBlurEffect.cpp
+    ${QNODEEDITOR_DIR}/src/ConnectionGeometry.cpp
+    ${QNODEEDITOR_DIR}/src/ConnectionGraphicsObject.cpp
+    ${QNODEEDITOR_DIR}/src/ConnectionPainter.cpp
+    ${QNODEEDITOR_DIR}/src/ConnectionState.cpp
+    ${QNODEEDITOR_DIR}/src/ConnectionStyle.cpp
+    ${QNODEEDITOR_DIR}/src/DataModelRegistry.cpp
+    ${QNODEEDITOR_DIR}/src/FlowScene.cpp
+    ${QNODEEDITOR_DIR}/src/FlowView.cpp
+    ${QNODEEDITOR_DIR}/src/FlowViewStyle.cpp
+    ${QNODEEDITOR_DIR}/src/Node.cpp
+    ${QNODEEDITOR_DIR}/src/NodeConnectionInteraction.cpp
+    ${QNODEEDITOR_DIR}/src/NodeDataModel.cpp
+    ${QNODEEDITOR_DIR}/src/NodeGeometry.cpp
+    ${QNODEEDITOR_DIR}/src/NodeGraphicsObject.cpp
+    ${QNODEEDITOR_DIR}/src/NodePainter.cpp
+    ${QNODEEDITOR_DIR}/src/NodeState.cpp
+    ${QNODEEDITOR_DIR}/src/NodeStyle.cpp
+    ${QNODEEDITOR_DIR}/src/Properties.cpp
+    ${QNODEEDITOR_DIR}/src/StyleCollection.cpp
+)
+set(QNODEEDITOR_INCLUDE_PATH
+    ${QNODEEDITOR_DIR}/src/
+    ${QNODEEDITOR_DIR}/include/
+    ${QNODEEDITOR_DIR}/include/nodes/
+    ${QNODEEDITOR_DIR}/include/nodes/internal
+)
--- cmake/ssl.cmake
+++
@@ -1,1 +0,0 @@
-find_package(OpenSSL REQUIRED)
