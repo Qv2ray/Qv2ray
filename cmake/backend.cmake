3faf40e2be96014b86ba10f15f4565ce96e82760
Guobang Bi <373318510@qq.com>
2020-03-07 15:39:02 +0800

    add backend.cmake

---
+++ cmake/backend.cmake
@@ -0,0 +1,38 @@
+if(DEFINED USE_GRPC)
+    add_definitions(-DWITH_LIB_GRPCPP)
+    find_program(GRPC_CPP_PLUGIN grpc_cpp_plugin)
+    
+    find_library(GRPC_LIBRARY NAMES grpc)
+    find_library(GRPCPP_LIBRARY NAMES grpc++)
+
+    if(UNIX AND NOT APPLE)
+        set(QV2RAY_BACKEND_LIBRARIES ${GRPCPP_LIBRARY} ${GRPC_LIBRARY})
+    elseif(APPLE)
+        find_library(UPB_LIBRARY NAMES upb)
+        set(QV2RAY_BACKEND_LIBRARIES ${GRPCPP_LIBRARY} ${GRPC_LIBRARY} ${UPB_LIBRARY})
+    elseif(WIN32)
+        find_library(ABSL_BASE NAMES absl_base)
+        find_library(ABSL_STR NAMES absl_strings)
+        find_library(ABSL_THROW_DELEGATE NAMES absl_throw_delegate)
+        set(QV2RAY_BACKEND_LIBRARIES ${GRPCPP_LIBRARY} ${GRPC_LIBRARY} ${ABSL_BASE} ${ABSL_STR} ${ABSL_THROW_DELEGATE})
+    endif()
+
+    set(API_PROTO "${CMAKE_SOURCE_DIR}/tools/v2ray_api.proto")
+    set(API_GRPC_SRCS "${CMAKE_CURRENT_BINARY_DIR}/v2ray_api.pb.cc")
+    set(API_GRPC_HDRS "${CMAKE_CURRENT_BINARY_DIR}/v2ray_api.pb.h")
+    add_custom_command(
+        OUTPUT "${API_GRPC_SRCS}" "${API_GRPC_HDRS}"
+        COMMAND ${Protobuf_PROTOC_EXECUTABLE}
+        ARGS --grpc_out "${CMAKE_CURRENT_BINARY_DIR}"
+            --cpp_out "${CMAKE_CURRENT_BINARY_DIR}"
+            --plugin=protoc-gen-grpc="${GRPC_CPP_PLUGIN}"
+            "${API_PROTO}"
+        DEPENDS "${API_PROTO}")
+    )
+else()
+    if(UNIX AND NOT APPLE)
+        set(QV2RAY_BACKEND_LIBRARIES ${CMAKE_SOURCE_DIR}/libs/libqvb-linux64.a)
+    elseif(APPLE)
+        set(QV2RAY_BACKEND_LIBRARIES ${CMAKE_SOURCE_DIR}/libs/libqvb-darwin.a)
+    endif()
+endif()
