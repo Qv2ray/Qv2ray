1b2318eff7ee7ee4be0ec60604dac9a1998ef4ea
Guobang Bi <373318510@qq.com>
2020-03-07 16:50:45 +0800

    add missing sources

--- cmake/backend.cmake
+++ cmake/backend.cmake
@@ -18,10 +18,12 @@ if(NOT USE_LIBQVB)
 
     set(API_PROTO "${CMAKE_SOURCE_DIR}/tools/v2ray_api.proto")
     set(API_PROTO_PATH "${CMAKE_SOURCE_DIR}/tools")
-    set(API_GRPC_SRCS "${CMAKE_CURRENT_BINARY_DIR}/v2ray_api.pb.cc")
-    set(API_GRPC_HDRS "${CMAKE_CURRENT_BINARY_DIR}/v2ray_api.pb.h")
+    set(API_PROTO_SRCS "${CMAKE_CURRENT_BINARY_DIR}/v2ray_api.pb.cc")
+    set(API_PROTO_HDRS "${CMAKE_CURRENT_BINARY_DIR}/v2ray_api.pb.h")
+    set(API_GRPC_SRCS "${CMAKE_CURRENT_BINARY_DIR}/v2ray_api.grpc.pb.cc")
+    set(API_GRPC_HDRS "${CMAKE_CURRENT_BINARY_DIR}/v2ray_api.grpc.pb.h")
     add_custom_command(
-        OUTPUT "${API_GRPC_SRCS}" "${API_GRPC_HDRS}"
+        OUTPUT "${API_GRPC_SRCS}" "${API_GRPC_HDRS}" "${API_PROTO_HDRS}" "${API_PROTO_SRCS}"
         COMMAND ${Protobuf_PROTOC_EXECUTABLE}
         ARGS --grpc_out "${CMAKE_CURRENT_BINARY_DIR}"
             --cpp_out "${CMAKE_CURRENT_BINARY_DIR}"
