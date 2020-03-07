4656e7fa29068f2c288f80a6c682c9106e42900a
Qv2ray-dev <59914293+Qv2ray-dev@users.noreply.github.com>
2020-03-07 17:12:33 +0800

    cmake: auto find translation files

--- cmake/translations.cmake
+++ cmake/translations.cmake
@@ -1,3 +1,4 @@
 find_package(Qt5 COMPONENTS LinguistTools)
 set(TRANSLATIONS_DIR ${CMAKE_SOURCE_DIR}/translations)
-qt5_create_translation(QM_FILES ${TRANSLATIONS_DIR}/en_US.ts ${TRANSLATIONS_DIR}/ja_JP.ts ${TRANSLATIONS_DIR}/ru_RU.ts ${TRANSLATIONS_DIR}/zh_CN.ts)
+file(GLOB TRANSLATIONS_TS ${TRANSLATIONS_DIR}/**.ts)
+qt5_create_translation(QM_FILES ${TRANSLATIONS_TS})
