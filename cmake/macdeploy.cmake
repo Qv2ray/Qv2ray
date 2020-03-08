a349bb21c612ef70568dc394d36dee4aad80bcfe
Qv2ray-dev <59914293+Qv2ray-dev@users.noreply.github.com>
2020-03-08 17:49:05 +0800

    cmake: CI: auto deployment

--- cmake/macdeploy.cmake
+++ cmake/macdeploy.cmake
@@ -32,30 +32,9 @@ install(TARGETS ${PROJECT_NAME}
     )
 install(FILES ${CMAKE_SOURCE_DIR}/assets/info.plist DESTINATION "${prefix}" )
 install(FILES ${CMAKE_SOURCE_DIR}/assets/icons/qv2ray.icns DESTINATION "${INSTALL_CMAKE_DIR}" )
+install(FILES ${QM_FILES}
+    DESTINATION ${INSTALL_CMAKE_DIR}/lang
+)
 
 # Note Mac specific extension .app
 set(APPS "\$ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/${PROJECT_NAME}.app")
-
-# Directories to look for dependencies
-set(DIRS "${CMAKE_BINARY_DIR}")
-
-# Path used for searching by FIND_XXX(), with appropriate suffixes added
-if(CMAKE_PREFIX_PATH)
-    foreach(dir ${CMAKE_PREFIX_PATH})
-        list(APPEND DIRS "${dir}/bin" "${dir}/lib")
-    endforeach()
-endif()
-
-# Append Qt's lib folder which is two levels above Qt5Widgets_DIR
-list(APPEND DIRS "${Qt5Widgets_DIR}/../..")
-list(APPEND DIRS "/usr/local/lib")
-list(APPEND DIRS "/usr/lib")
-
-include(InstallRequiredSystemLibraries)
-
-message(STATUS "APPS: ${APPS}")
-message(STATUS "QT_PLUGINS: ${QT_PLUGINS}")
-message(STATUS "DIRS: ${DIRS}")
-
-install(CODE "include(BundleUtilities)
-    fixup_bundle(\"${APPS}\" \"${QT_PLUGINS}\" \"${DIRS}\")")
