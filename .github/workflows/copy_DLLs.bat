bd63164781945562de20991fd8cbdf351fdc0f2c
Qv2ray-dev <59914293+Qv2ray-dev@users.noreply.github.com>
2020-03-08 20:12:42 +0800

    cmake: CI: remove unused action step

--- .github/workflows/copy_DLLs.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Copying DLLs for Windows
-forfiles /s /p %GITHUB_WORKSPACE%\libs\x86-windows\bin\ /m "*.dll" /c "cmd.exe /c copy @file %GITHUB_WORKSPACE%\build\release\"
-forfiles /s /p %GITHUB_WORKSPACE%\libs\x64-windows\bin\ /m "*.dll" /c "cmd.exe /c copy @file %GITHUB_WORKSPACE%\build\release\"
-exit 0
