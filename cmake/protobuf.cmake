c29aeb438bd2e50a3f2814aae93630dfb463c084
Guobang Bi <373318510@qq.com>
2020-03-08 14:35:08 +0800

    linking protobuf statically

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -1,4 +1,7 @@
 find_package(Protobuf REQUIRED)
+if(MSVC)
+    set(Protobuf_USE_STATIC_LIBS ON)
+endif()
 protobuf_generate_cpp(PROTO_SRCS PROTO_HDRS ${CMAKE_SOURCE_DIR}/tools/v2ray_geosite.proto)
 set(QV2RAY_PROTOBUF_LIBRARY 
     protobuf::libprotobuf
