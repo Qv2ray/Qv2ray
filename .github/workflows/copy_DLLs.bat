6726046a6cdabcb99e72736c677278d11e13689a
Qv2ray-dev <59914293+Qv2ray-dev@users.noreply.github.com>
2020-03-08 16:32:20 +0800

    Reeeevert "cmake: CI: update Windows build script"
    This reverts commit 9c69552ff9b71b41f3811586e4ec2a2b8c5c3aa7.

--- .github/workflows/copy_DLLs.bat
+++ .github/workflows/copy_DLLs.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Copying DLLs for Windows
-forfiles /s /p %GITHUB_WORKSPACE%\libs\x86-windows\installed\x86-windows\bin\ /m "*.dll" /c "cmd.exe /c copy @file %GITHUB_WORKSPACE%\build\release\"
-forfiles /s /p %GITHUB_WORKSPACE%\libs\x64-windows\installed\x64-windows\bin\ /m "*.dll" /c "cmd.exe /c copy @file %GITHUB_WORKSPACE%\build\release\"
+forfiles /s /p %GITHUB_WORKSPACE%\libs\x86-windows\bin\ /m "*.dll" /c "cmd.exe /c copy @file %GITHUB_WORKSPACE%\build\release\"
+forfiles /s /p %GITHUB_WORKSPACE%\libs\x64-windows\bin\ /m "*.dll" /c "cmd.exe /c copy @file %GITHUB_WORKSPACE%\build\release\"
 exit 0
