692d9229c7812e5a25d367ce9bdc102d0ca60db2
DuckSoft <realducksoft@gmail.com>
2020-03-10 21:38:34 +0800

    [impl] windows implementation of darkmode detector

--- src/components/darkmode/DarkmodeDetector_win.cpp
+++ src/components/darkmode/DarkmodeDetector_win.cpp
@@ -1,12 +1,15 @@
 #include "DarkmodeDetector.hpp"
 #ifdef Q_OS_WIN32
+    #include <QSettings>
 
 namespace Qv2ray::components::darkmode
 {
+    // Referenced from github.com/keepassxreboot/keepassxc. Licensed under GPL2/3.
+    // Copyright (C) 2020 KeePassXC Team <team@keepassxc.org>
     bool detectDarkmode()
     {
-        // TODO: expand this stub
-        return false;
+        QSettings settings(R"(HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize)", QSettings::NativeFormat);
+        return settings.value("AppsUseLightTheme", 1).toInt() == 0;
     }
 } // namespace Qv2ray::components::darkmode
 
