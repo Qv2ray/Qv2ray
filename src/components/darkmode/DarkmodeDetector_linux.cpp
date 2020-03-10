1fafee72b6baf54ad0177dfeb9e5f27d2f390f19
DuckSoft <realducksoft@gmail.com>
2020-03-10 21:34:33 +0800

    [impl] linux implementation of darkmode detector

--- src/components/darkmode/DarkmodeDetector_linux.cpp
+++ src/components/darkmode/DarkmodeDetector_linux.cpp
@@ -1,12 +1,19 @@
 #include "DarkmodeDetector.hpp"
 #ifdef Q_OS_LINUX
+    #include <QApplication>
+    #include <QStyle>
 
 namespace Qv2ray::components::darkmode
 {
+    // Referenced from github.com/keepassxreboot/keepassxc. Licensed under GPL2/3.
+    // Copyright (C) 2020 KeePassXC Team <team@keepassxc.org>
     bool detectDarkmode()
     {
-        // TODO: expand this stub
-        return false;
+        if (!qApp || !qApp->style())
+        {
+            return false;
+        }
+        return qApp->style()->standardPalette().color(QPalette::Window).toHsl().lightness() < 110;
     }
 } // namespace Qv2ray::components::darkmode
 
