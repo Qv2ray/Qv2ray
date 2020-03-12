ba32111169169d761808c3c4b3365ad189b47193
Qv2ray-dev <59914293+Qv2ray-dev@users.noreply.github.com>
2020-03-12 22:48:33 +0800

    refactor: some minor refactors to the main.cpp

--- src/core/settings/SettingsBackend.hpp
+++ src/core/settings/SettingsBackend.hpp
@@ -4,6 +4,7 @@ namespace Qv2ray::core::config
 {
     void SaveGlobalConfig(const Qv2rayConfig &conf);
     void SetConfigDirPath(const QString &path);
+    bool CheckSettingsPathAvailability(const QString &_path, bool checkExistingConfig);
 } // namespace Qv2ray::core::config
 
 using namespace Qv2ray::core;
