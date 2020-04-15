55f66e141c787cdc13612cd4d0b6e45ef99c4eda
Qv2ray-dev <59914293+Qv2ray-dev@users.noreply.github.com>
2020-04-15 23:20:31 +0800

    macOS: just a test

--- cmake/deployment.cmake
+++ cmake/deployment.cmake
@@ -60,14 +60,7 @@ if(BUILD_NSIS)
 endif()
 
 if(APPLE)
-    set(CPACK_GENERATOR "DragNDrop")
-    if(DS_STORE_SCRIPT)
-        set(CPACK_DMG_DS_STORE_SETUP_SCRIPT "${CMAKE_SOURCE_DIR}/cmake/CMakeDMGSetup.scpt")
-    else()
-        set(CPACK_DMG_DS_STORE "${CMAKE_SOURCE_DIR}/assets/DS_Store")
-    endif()
-    
-    set(CPACK_DMG_BACKGROUND_IMAGE "${CMAKE_SOURCE_DIR}/assets/CMakeDMGBackground.png")
+    CONFIGURE_FILE("package_dmg.json.in" "package_dmg.json" @ONLY)
 endif()
 
 include(CPack)
