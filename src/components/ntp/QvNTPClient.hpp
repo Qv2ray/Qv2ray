53a40fda87a15a276102d1c0b700f97e888cf1ec
DuckSoft <realducksoft@gmail.com>
2020-05-12 23:25:47 +0800

    adding license

--- src/components/ntp/QvNTPClient.hpp
+++ src/components/ntp/QvNTPClient.hpp
@@ -1,3 +1,20 @@
+/* This file from part of QNtp, a library that implements NTP protocol.
+ *
+ * Copyright (C) 2011 Alexander Fokin <apfokin@gmail.com>
+ *
+ * QNtp is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * QNtp is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with QNtp. If not, see <http://www.gnu.org/licenses/>. */
+
 #pragma once
 
 #include <QDateTime>
