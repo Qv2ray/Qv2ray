d4d39a5180c14829427efa75e8e8a4e6bfdd2ae9
Qv2ray-dev <59914293+Qv2ray-dev@users.noreply.github.com>
2020-05-15 23:39:23 +0800

    refactor: now use QJsonIO in tests

--- test/src/core/connection/TestParseSS.cpp
+++ test/src/core/connection/TestParseSS.cpp
@@ -1,20 +1,23 @@
-#include <QTest>
-#include "src/core/connection/Serialization.hpp"
 #include "libs/QJsonStruct/QJsonIO.hpp"
+#include "src/core/connection/Serialization.hpp"
 
-class ParseSSUrl: public QObject {
+#include <QTest>
+
+class ParseSSUrl : public QObject
+{
     Q_OBJECT
-private slots:
-    void t1() { 
+  private slots:
+    void t1()
+    {
         using namespace Qv2ray::core::connection::Serialization;
         QString err;
-        QString alias="ssurl1";
-        auto c=ss::Deserialize("ss://YmYtY2ZiOnRlc3RAMTkyLjE2OC4xMDAuMTo4ODg4", &alias, &err);
-        auto s=ShadowSocksServerObject::fromJson(c["outbounds"].toArray().first().toObject()["settings"].toObject()["servers"].toArray().first().toObject());
+        QString alias = "ssurl1";
+        auto c = ss::Deserialize("ss://YmYtY2ZiOnRlc3RAMTkyLjE2OC4xMDAuMTo4ODg4", &alias, &err);
+        auto s = ShadowSocksServerObject::fromJson(QJsonIO::GetValue(c, "outbounds", 0, "settings", "servers", 0));
         QVERIFY(s.address == "192.168.100.1");
         QVERIFY(s.port == 8888);
-        QVERIFY(s.password=="test");
-        QVERIFY(s.method=="bf-cfb");
+        QVERIFY(s.password == "test");
+        QVERIFY(s.method == "bf-cfb");
     }
 };
 
