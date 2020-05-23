b9b86efa0d0be7d4f37e5b041b2bfdf46bdd7d50
Qv2ray-dev <59914293+Qv2ray-dev@users.noreply.github.com>
2020-05-23 22:01:03 +0800

    fix: prevent reading geosite/geoip every time

--- src/components/geosite/QvGeositeReader.hpp
+++ src/components/geosite/QvGeositeReader.hpp
@@ -4,7 +4,7 @@
 namespace Qv2ray::components::geosite
 {
     QStringList ReadGeoSiteFromFile(const QString &filepath);
-}
+} // namespace Qv2ray::components::geosite
 
 using namespace Qv2ray::components;
 using namespace Qv2ray::components::geosite;
