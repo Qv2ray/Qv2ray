eb2966bd38dd1ce897a365f5b9fec9dac3613cf3
DuckVador <Lx3JQkmzRS@protonmail.com>
2020-07-08 19:31:37 +0800

    add submodule: uvw

--- cmake/libuv.cmake
+++ cmake/libuv.cmake
@@ -2,34 +2,34 @@ add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/libuv)
 add_library(uv::uv-static ALIAS uv_a)
 set_target_properties(uv_a PROPERTIES POSITION_INDEPENDENT_CODE 1)
 set(UVW_SOURCES
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/async.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/check.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/dns.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/emitter.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/fs.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/fs_event.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/fs_poll.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/idle.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/lib.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/loop.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/pipe.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/poll.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/prepare.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/process.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/signal.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/stream.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/tcp.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/thread.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/timer.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/tty.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/util.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/work.cpp
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/uvw/udp.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/async.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/check.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/dns.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/emitter.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/fs.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/fs_event.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/fs_poll.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/idle.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/lib.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/loop.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/pipe.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/poll.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/prepare.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/process.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/signal.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/stream.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/tcp.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/thread.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/timer.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/tty.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/util.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/work.cpp
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src/uvw/udp.cpp
 )
 add_library(UVW_LIB ${UVW_SOURCES})
 target_compile_definitions(UVW_LIB PUBLIC UVW_AS_LIB)
 target_link_libraries(UVW_LIB uv::uv-static)
 set(LibUV_LIBRARIES uv::uv-static UVW_LIB)
 set(LibUV_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/libuv/include
-       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw
+       ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/uvw/src
 )
