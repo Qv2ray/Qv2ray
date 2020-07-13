93cb434765ca1e09cdf68f063a6bdcc7e48fc7d6
QwQ <59914293+Qv2ray-dev@users.noreply.github.com>
2020-07-14 00:53:33 +0800

    fix: resolved #777

--- src/core/handler/ConfigHandler.cpp
+++ src/core/handler/ConfigHandler.cpp
@@ -616,7 +616,8 @@ namespace Qv2ray::core::handler
             filteredConnections.count() > 5 ||
             QvMessageBoxAsk(nullptr, tr("Update Subscription"),
                             tr("%1 out of %n entrie(s) have been filtered out, do you want to continue?", "", _newConnections.count())
-                                .arg(filteredConnections.count())) == QMessageBox::Yes;
+                                    .arg(filteredConnections.count()) +
+                                NEWLINE + GetDisplayName(id)) == QMessageBox::Yes;
 
         for (const auto &config : useFilteredConnections ? filteredConnections : _newConnections)
         {
@@ -666,7 +667,7 @@ namespace Qv2ray::core::handler
                                 tr("Update Subscription"),
                                 tr("There're %n connection(s) in the group that do not belong the current subscription (any more).", "",
                                    originalConnectionIdList.count()) +
-                                    NEWLINE + tr("Would you like to remove them?")) == QMessageBox::Yes;
+                                    NEWLINE + GetDisplayName(id) + NEWLINE + tr("Would you like to remove them?")) == QMessageBox::Yes;
             if (needContinue)
             {
                 LOG(MODULE_CORE_HANDLER, "Removed old connections not have been matched.")
