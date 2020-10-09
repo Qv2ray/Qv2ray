5746f5f106da0af4d25750d0f664d558273048a3
QxQ <59914293+U-v-U@users.noreply.github.com>
2020-10-09 10:44:48 +0800

    F I X E D    T H E     B U I L D

--- cmake/platforms/prefixes.cmake
+++ cmake/platforms/prefixes.cmake
@@ -11,6 +11,19 @@ list(APPEND CMAKE_PROGRAM_PATH
 
 message("CMAKE_PROGRAM_PATH=${CMAKE_PROGRAM_PATH}")
 
+# From vcpkg
+#If CMake does not have a mapping for MinSizeRel and RelWithDebInfo in imported targets
+#it will map those configuration to the first valid configuration in CMAKE_CONFIGURATION_TYPES or the targets IMPORTED_CONFIGURATIONS.
+#In most cases this is the debug configuration which is wrong.
+if(NOT DEFINED CMAKE_MAP_IMPORTED_CONFIG_MINSIZEREL)
+    set(CMAKE_MAP_IMPORTED_CONFIG_MINSIZEREL "MinSizeRel;Release;")
+    message(STATUS "CMAKE_MAP_IMPORTED_CONFIG_MINSIZEREL set to MinSizeRel;Release;")
+endif()
+if(NOT DEFINED CMAKE_MAP_IMPORTED_CONFIG_RELWITHDEBINFO)
+    set(CMAKE_MAP_IMPORTED_CONFIG_RELWITHDEBINFO "RelWithDebInfo;Release;")
+    message(STATUS "CMAKE_MAP_IMPORTED_CONFIG_RELWITHDEBINFO set to RelWithDebInfo;Release;")
+endif()
+
 if(CMAKE_BUILD_TYPE MATCHES "^[Dd][Ee][Bb][Uu][Gg]$" OR NOT DEFINED CMAKE_BUILD_TYPE)
     #Debug build: Put Debug paths before Release paths.
     list(APPEND CMAKE_PREFIX_PATH
