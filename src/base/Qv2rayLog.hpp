94d701ce91c7b0ee18577625e14957c882a24f25
QxQ <59914293+U-v-U@users.noreply.github.com>
2020-10-13 18:04:14 +0800

    fix: fixed log and tests issue

--- src/base/Qv2rayLog.hpp
+++ src/base/Qv2rayLog.hpp
@@ -13,7 +13,7 @@
 #endif
 
 #define NEWLINE "\r\n"
-#define ___LOG_EXPAND(___x) , QPair(#___x, [&] { return ___x; }())
+#define ___LOG_EXPAND(___x) , QPair(std::string(#___x), [&] { return ___x; }())
 #define A(...) FOREACH_CALL_FUNC(___LOG_EXPAND, __VA_ARGS__)
 
 #ifdef QT_DEBUG
