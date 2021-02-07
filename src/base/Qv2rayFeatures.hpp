593eae11aaf5508115f284065552f29207b24c38
QxQ <59914293+U-v-U@users.noreply.github.com>
2021-02-07 21:26:26 +0800

    turned off all optional features

--- src/base/Qv2rayFeatures.hpp
+++ src/base/Qv2rayFeatures.hpp
@@ -4,15 +4,15 @@
 // clang-format off
 
 // Kernel Options
-#define QVFEATURE_kernel_check_abi               1
-#define QVFEATURE_kernel_check_permission        1
-#define QVFEATURE_kernel_check_output            1
-#define QVFEATURE_kernel_check_filename          1
-#define QVFEATURE_kernel_set_permission          1
+#define QVFEATURE_kernel_check_abi              -1
+#define QVFEATURE_kernel_check_permission       -1
+#define QVFEATURE_kernel_check_output           -1
+#define QVFEATURE_kernel_check_filename         -1
+#define QVFEATURE_kernel_set_permission         -1
 
 // UI Options
 #define QVFEATURE_ui_has_import_qrcode          -1
-#define QVFEATURE_ui_has_store_state             1
+#define QVFEATURE_ui_has_store_state            -1
 
 // Ultilities
 #define QVFEATURE_util_has_ntp                  -1
