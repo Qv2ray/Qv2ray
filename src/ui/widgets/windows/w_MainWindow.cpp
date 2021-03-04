9750369d227c0333eb3b11b632e3b522d6f74071
DuckVador <Lx3JQkmzRS@protonmail.com>
2021-03-04 22:40:51 +0800

    fix a display bug of RealPing

--- src/ui/widgets/windows/w_MainWindow.cpp
+++ src/ui/widgets/windows/w_MainWindow.cpp
@@ -460,7 +460,7 @@ void MainWindow::keyReleaseEvent(QKeyEvent *e)
 void MainWindow::changeEvent(QEvent *e)
 {
 
-    if(e->type() == QEvent::WindowStateChange)
+    if (e->type() == QEvent::WindowStateChange)
     {
         MWToggleVisibilitySetText();
     }
@@ -973,7 +973,7 @@ void MainWindow::Action_TestRealLatency()
         const auto widget = GetIndexWidget(current);
         if (!widget)
             continue;
-        if (widget->IsConnection())
+        if (widget->IsConnection() && ConnectionManager->IsConnected(widget->Identifier()))
             ConnectionManager->StartLatencyTest(widget->Identifier().connectionId, REALPING);
     }
 }
