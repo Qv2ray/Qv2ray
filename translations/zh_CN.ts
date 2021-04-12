a35a5f90ebe30e73a6c07d1a945999f898c71b01
QxQ <59914293+U-v-U@users.noreply.github.com>
2021-04-12 23:15:02 +0800

    New translations en_US.ts (Chinese Simplified)

--- translations/zh_CN.ts
+++ translations/zh_CN.ts
@@ -3183,6 +3183,14 @@ Maybe you have downloaded the wrong core?</source>
       <source>grpc</source>
       <translation>格子</translation>
     </message>
+    <message>
+      <source>Max Early Data</source>
+      <translation>最大早期数据</translation>
+    </message>
+    <message>
+      <source>Browser Forwarding</source>
+      <translation>浏览器转发中</translation>
+    </message>
   </context>
   <context>
     <name>w_GroupManager</name>
