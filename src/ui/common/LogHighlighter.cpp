19ef0b184563a637eff62920caefc3a4c68ccb14
QxQ <59914293+U-v-U@users.noreply.github.com>
2021-04-12 22:19:21 +0800

    fix v2ray 4.37 log highlighting

--- src/ui/common/LogHighlighter.cpp
+++ src/ui/common/LogHighlighter.cpp
@@ -92,7 +92,7 @@ namespace Qv2ray::ui
         highlightingRules.append(rule);
         //
         v2rayComponentFormat.setForeground(darkMode ? darkGreenColor : Qt::darkYellow);
-        rule.pattern = QRegularExpression(R"( (github.com\/v2fly\/v2ray-core)[\/\w*]*: )");
+        rule.pattern = QRegularExpression(R"( (\w+\/)+\w+: )");
         rule.format = v2rayComponentFormat;
         highlightingRules.append(rule);
         //
