c413ed83d9059764f7dbdbe80bbddb632318d75a
QxQ <59914293+U-v-U@users.noreply.github.com>
2021-04-13 14:30:42 +0800

    New translations en_US.ts (Cantonese)

--- translations/yue.ts
+++ translations/yue.ts
@@ -817,10 +817,6 @@ This entry is ignored by V2Ray core when using DoH servers.</source>
       <translation type="unfinished">  Log</translation>
     </message>
     <message>
-      <source>Clear log</source>
-      <translation type="unfinished">Clear log</translation>
-    </message>
-    <message>
       <source>Not Connected</source>
       <translation type="unfinished">Not Connected</translation>
     </message>
