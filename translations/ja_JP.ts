77b59988483513048b25711e22e4652283724260
QxQ <59914293+U-v-U@users.noreply.github.com>
2021-04-17 20:35:27 +0800

    New translations en_US.ts (Japanese)

--- translations/ja_JP.ts
+++ translations/ja_JP.ts
@@ -7,6 +7,18 @@
       <source>Form</source>
       <translation>Form</translation>
     </message>
+    <message>
+      <source>Show / Hide</source>
+      <translation type="unfinished">Show / Hide</translation>
+    </message>
+    <message>
+      <source>Selector</source>
+      <translation type="unfinished">Selector</translation>
+    </message>
+    <message>
+      <source>Strategy</source>
+      <translation>ルール</translation>
+    </message>
   </context>
   <context>
     <name>CertificateItemWidget</name>
@@ -118,6 +130,10 @@
     <message>
       <source>Form</source>
       <translation>Form</translation>
+    </message>
+    <message>
+      <source>Port</source>
+      <translation>ポート</translation>
     </message>
   </context>
   <context>
@@ -666,12 +682,16 @@ This entry is ignored by V2Ray core when using DoH servers.</source>
       <translation>FakeDNS</translation>
     </message>
     <message>
-      <source>Destination Override:</source>
-      <translation>目標上書き:</translation>
-    </message>
-    <message>
       <source>Metadata Only</source>
       <translation>メタデータのみ</translation>
+    </message>
+    <message>
+      <source>FakeDNS-Others</source>
+      <translation type="unfinished">FakeDNS-Others</translation>
+    </message>
+    <message>
+      <source>Destination Override</source>
+      <translation>目標上書き</translation>
     </message>
   </context>
   <context>
@@ -2771,6 +2791,30 @@ Maybe you have downloaded the wrong core?</source>
     <message>
       <source>Reference Connection</source>
       <translation>参照接続</translation>
+    </message>
+    <message>
+      <source>Misc</source>
+      <translation type="unfinished">Misc</translation>
+    </message>
+    <message>
+      <source>Browser Forwarder</source>
+      <translation type="unfinished">Browser Forwarder</translation>
+    </message>
+    <message>
+      <source>Listening Address</source>
+      <translation>リスニングアドレス</translation>
+    </message>
+    <message>
+      <source>:Listening Port</source>
+      <translation type="unfinished">:Listening Port</translation>
+    </message>
+    <message>
+      <source> Observatory</source>
+      <translation type="unfinished"> Observatory</translation>
+    </message>
+    <message>
+      <source>Subject Selector</source>
+      <translation type="unfinished">Subject Selector</translation>
     </message>
   </context>
   <context>
