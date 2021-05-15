b5fcc4db09b0dce2382ac25ea6d271e061044ba8
database64128 <free122448@hotmail.com>
2021-05-16 01:11:24 +0800

    🖇️ Link against OpenSSL

--- cmake/libcurl.cmake
+++ cmake/libcurl.cmake
@@ -2,4 +2,12 @@ if(WIN32)
     set(CURL_LIBRARIES libcurl libssl libcrypto zlib)
 endif()
 
+find_package(OpenSSL REQUIRED)
 find_package(CURL REQUIRED)
+
+if(APPLE)
+    include_directories(${OPENSSL_ROOT_DIR}/include)
+    set(OPENSSL_CRYPTO_LIBRARY ${OPENSSL_ROOT_DIR}/lib/libcrypto.dylib CACHE FILEPATH "" FORCE)
+    set(OPENSSL_SSL_LIBRARY ${OPENSSL_ROOT_DIR}/lib/libssl.dylib CACHE FILEPATH "" FORCE)
+    message(STATUS "OpenSSL Version: ${OPENSSL_VERSION} ${OPENSSL_INCLUDE_DIR} ${OPENSSL_LIBRARIES}")
+endif()
