aa0e04473ac0e8a39a9edf26cb1726e39aef037c
DuckSoft <realducksoft@gmail.com>
2021-05-16 19:21:32 +0800

    feat: add support for tlsObject.pinnedPeerCertificateChainSha256 (#1489)
    * feat: add support for tlsObject.pinnedPeerCertificateChainSha256
    
    * feat: re-implementation of ChainSha256Editor
    
    Thanks @Sinofine
    
    * chores: added backward compatibility code
    
    Co-authored-by: Sinofine Lotus <i@sinofine.me>

--- src/ui/widgets/editors/StreamSettingsWidget.hpp
+++ src/ui/widgets/editors/StreamSettingsWidget.hpp
@@ -72,6 +72,8 @@ class StreamSettingsWidget
     // gRPC
     void on_grpcServiceNameTxt_textEdited(const QString &arg1);
     void on_grpcModeCB_currentIndexChanged(int arg1);
+    //
+    void on_pinnedPeerCertificateChainSha256Btn_clicked();
 
   private:
     Qv2ray::Models::StreamSettingsObject stream;
---
+++ src/ui/widgets/editors/w_ChainSha256Editor.cpp
@@ -0,0 +1,64 @@
+#include "w_ChainSha256Editor.hpp"
+
+#include "ui/widgets/common/WidgetUIBase.hpp"
+#include "utils/QvHelpers.hpp"
+
+#include <QDesktopServices>
+
+ChainSha256Editor::ChainSha256Editor(QWidget *parent, const QList<QString> &chain) : QDialog(parent)
+{
+    setupUi(this);
+    chainSha256Edit->setPlainText(chain.join("\r\n"));
+    QvMessageBusConnect(ChainSha256Editor);
+}
+
+QvMessageBusSlotImpl(ChainSha256Editor)
+{
+    switch (msg)
+    {
+        MBShowDefaultImpl;
+        MBHideDefaultImpl;
+        MBRetranslateDefaultImpl;
+        case UPDATE_COLORSCHEME: break;
+    }
+}
+
+void ChainSha256Editor::accept()
+{
+    const auto newChain = ChainSha256Editor::convertFromString(chainSha256Edit->toPlainText());
+    if (const auto err = ChainSha256Editor::validateError(newChain); err)
+    {
+        QvMessageBoxWarn(this, tr("Invalid Certificate Hash Chain"), *err);
+        return;
+    }
+    this->chain = newChain;
+    this->QDialog::accept();
+}
+
+QList<QString> ChainSha256Editor::convertFromString(const QString &&str)
+{
+    const static QRegExp newLine("[\r\n]");
+#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
+    return str.split(newLine, Qt::SplitBehaviorFlags::SkipEmptyParts);
+#else
+    return str.split(newLine, QString::SkipEmptyParts);
+#endif
+}
+
+std::optional<QString> ChainSha256Editor::validateError(const QList<QString> &newChain)
+{
+    const static QRegExp sha256("[0-9a-fA-F]{64}");
+
+    for (const auto &entry : newChain)
+    {
+        if (!sha256.exactMatch(entry))
+            return tr("invalid SHA256: %1").arg(entry);
+    }
+
+    return std::nullopt;
+}
+
+void ChainSha256Editor::on_buttonBox_helpRequested()
+{
+    QDesktopServices::openUrl(QUrl("https://www.v2fly.org/config/transport.html#tlsobject"));
+}
