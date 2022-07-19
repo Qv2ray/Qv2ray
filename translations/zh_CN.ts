a987b2755427a001f01d7492afc37d6a4e74cb37
DuckSoft <realducksoft@gmail.com>
2022-07-19 18:55:54 +0800

    New translations en_US.ts (Chinese Simplified)

--- translations/zh_CN.ts
+++ translations/zh_CN.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh-CN" sourcelanguage="en">
+<TS version="2.1" language="zh_CN" sourcelanguage="en">
   <context>
     <name>BalancerWidget</name>
     <message>
